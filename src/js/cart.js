d6ae1c70782a70b43a9932be9e16fed0020c6886
sarahandressa <sarahagdiaz@yahoo.com.br>
2025-07-15 23:15:08 -0300

    Fix Empty Card Error

--- src/js/cart.js
+++ src/js/cart.js
@@ -27,7 +27,7 @@ function cartItemTemplate(item) {
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__color">${item.Colors?.[0]?.ColorName || "N/A"}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
   <span class="remove-item" data-id="${item.Id}">❌</span>
