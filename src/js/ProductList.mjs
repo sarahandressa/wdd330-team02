b34522bb0ffe07cff8e777b9f53bfafc5dd629eb
Vicore123 <viniciuslimaalves123@gmail.com>
2025-07-18 10:37:01 -0300

    fixing cart icon and paths

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -5,22 +5,22 @@ function productCardTemplate(product) {
   const discountPercent = isDiscounted
     ? Math.round(((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100)
     : 0;
-
-
-  return `<li class="product-card">
-    <a href="product_pages/?product=${product.Id}">
-      <div class="image-container">
-        <img src="${product.Image}" alt="Image of ${product.Name}">
-        ${isDiscounted ? `<div class="discount-badge">-${discountPercent}%</div>` : ""}
-      </div>  
-      <h2 class="card__brand">${product.Brand}</h2>
-      <h3 class="card__name">${product.Name}</h3>
-      <p class="product-card__price">
-      ${isDiscounted ? `<span class="old-price">$${product.SuggestedRetailPrice.toFixed(2)}</span>` : ""}
-      <span class="final-price">$${product.FinalPrice.toFixed(2)}</span>
-      </p>
-    </a>
-  </li>`;
+  return `
+    <li class="product-card">
+      <a href="../../product_pages/?product=${product.Id}">
+        <div class="image-container">
+          <img src="${product.Image}" alt="Image of ${product.Name}">
+          ${isDiscounted ? `<div class="discount-badge">-${discountPercent}%</div>` : ""}
+        </div>  
+        <h2 class="card__brand">${product.Brand}</h2>
+        <h3 class="card__name">${product.Name}</h3>
+        <p class="product-card__price">
+        ${isDiscounted ? `<span class="old-price">$${product.SuggestedRetailPrice.toFixed(2)}</span>` : ""}
+        <span class="final-price">$${product.FinalPrice.toFixed(2)}</span>
+        </p>
+      </a>
+    </li>
+  `;
 }
 
 
